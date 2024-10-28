f4fc45de14e6675ac15b6fac0ccc46acdebfa2e6
Vitr Diógenes <vitor.diogens@gmail.com>
2024-10-28 16:40:19 -0300

    Em andamento

--- src/Components/Api/Api.jsx
+++ src/Components/Api/Api.jsx
@@ -1,4 +1,7 @@
+import { useState } from "react";
 export default function Api({ nome }) {
+  const [personagem, setPersonagem] = useState("");
+
   let url = `https://swapi.dev/api/people/?search=${nome}`;
   const busca = (nome) => {
     fetch(url)
@@ -6,12 +9,10 @@ export default function Api({ nome }) {
         let contentType = resposta.headers.get("content-type");
         if (contentType && contentType.indexOf("application/json") !== -1) {
           return resposta.json().then((json) => {
-              if (json.results[0].name == nome) {
-                console.log("Fazer a busca no json pelo " + nome);
-                console.log(json.results[0].name)
-                
-              }
-            
+            if (json.results[0].name == nome) {
+              setPersonagem(json.results[0]);
+              console.log(json.results[0]);
+            }
           });
         }
       })
@@ -19,6 +20,20 @@ export default function Api({ nome }) {
         console.error("Erro na requisição" + error);
       });
   };
-
-  return busca(nome);
+  return (
+    <>
+      {busca(nome)}
+      <div className="max-w-2xl m-auto p-5 px-8 rounded-lg shadow-[3px_3px_10px]">
+        <ul>
+          <li>Nome: {personagem.name}</li>
+          <li>Altura: {personagem.height} cm</li>
+          <li>Massa: {personagem.mass} kg</li>
+          <li>Cor do cabelo: {personagem.hair_color}</li>
+          <li>Cor da pele: {personagem.eye_color}</li>
+          <li>Ano de aniversário: {personagem.birth_year} </li>
+          <li>Gênero: {personagem.gender}</li>
+        </ul>
+      </div>
+    </>
+  );
 }
--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -2,14 +2,17 @@ import { useState } from "react";
 import Api from "../Api/Api";
 function App() {
   const [nome, setNome] = useState(null)
+  
 
 
 
   return (
     <>
       <h1 className="text-center m-2 text-3xl font-bold underline">Hello world!</h1>
-      <input type="text" className="border" onChange={(e)=> {setNome(e.target.value)}} />
-    <Api nome={nome}/>
+      <div className="max-w-2xl text-center m-auto p-5 px-8 shadow" >
+      <input type="text" className="border" onKeyUp={(e)=> {setNome(e.target.value)}} placeholder="Faça sua pesquisa" />
+      </div>
+    <Api nome={nome} />
     </>
   );
 }
