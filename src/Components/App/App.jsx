150326174f54524fdbe7a182e813085ac37c09d9
Vitr Diógenes <vitor.diogens@gmail.com>
2024-11-18 16:36:03 -0300

    Tá dificil, mas vou resolver

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -4,56 +4,35 @@ import { useState,useEffect } from "react";
 function App() {
 
   const [carregando, setCarregando] = useState(false);
-
-  const [busca, setBusca] = useState();
-
+  const [busca, setBusca] = useState("");
   const [personagens, setPersonagens] = useState([]);
+  const [tipo, setTipo] = useState(""); 
 
 
   const getStar = async (search,tipo) => {
     setCarregando(true);
     const resposta = await fetch(`https://swapi.dev/api/${tipo}/?search=${search}`);
-    const dados = await resposta.json();
-    setPersonagens((prevPersonagens) => [
-      ...prevPersonagens,
-      ...dados.results.map((personagem) => {
-        return {
-          Id: Math.floor(Math.random()*(100-0)),
-          Nome:"Nome: " + personagem.name,
-          //Personagem
-          Altura:"Altura: " +  personagem.height,
-          Massa:"Massa: " + personagem.mass,
-          Cabelo: "Cor do cabelo: " + personagem.hair_color,
-          Genero: "Gênero: " + personagem.gender,
-          //Planeta
-          Rotacao:"Rotação: " + personagem.rotation_period,
-          Orbita: "Orbita: " + personagem.orbital_period ,
-          Diametro: "Diametro: " + personagem.diameter,
-          Clima: "Clima: " + personagem.climate,
-          //Nave
-          Modelo: "Modelo: " + personagem.model,
-          Fabricante: "Fabricante: " + personagem.manufacturer,
-          Comprimento: "Comprimento: " + personagem.length,
-          Capacidade:"Capacidade: " + personagem.cargo_capacity,  
+    const json = await resposta.json();
 
-          URL:"URL: " + personagem.url
-
-        };
-
-      })
-    ])
-       setCarregando(false);
+    if (json.results) {
+      setPersonagens((prevPersonagens) => [
+        ...prevPersonagens,
+        ...json.results
+      ]);
+    }
+    setCarregando(false);
   };
 
-   useEffect(() => {
-     getStar(busca);
-   }, []);
-
+  useEffect(() => {
+    if (busca && tipo) {
+      getStar(busca, tipo);
+    }
+  }, [busca, tipo]);
   return (
     <>
 
       <h1 className="text-center text-3xl font-bold underline">Hello world!</h1>
-      <Lista  carregando={carregando} personagens={personagens} setPersonagens={setPersonagens} getStar={getStar}/>
+      <Lista  carregando={carregando} personagens={personagens} setPersonagens={setPersonagens} getStar={getStar} setBusca={setBusca} setTipo={setTipo}/>
     </>
   );
 }
--- src/Components/Lista/Lista.jsx
+++ src/Components/Lista/Lista.jsx
@@ -1,85 +1,100 @@
-import { useRef } from "react"
+import { useRef } from "react";
 
-function Lista({personagens,carregando,getStar,setPersonagens}) {
+function Lista({ personagens, carregando, getStar, setBusca, setTipo }) {
+  const input = useRef("");
+  const tipoRef = useRef("");
 
-    const input = useRef("")
-    const type  = useRef("")
-    
+  const handleClick = () => {
+    const valorBusca = input.current.value;
+    const valorTipo = tipoRef.current.value;
+    setTipo(valorTipo); 
+    setBusca(valorBusca); 
+    //getStar(valorBusca, valorType);
+    input.current.value = "";
+  };
 
-    const handleClick =  ()=>{
-      const valorBusca = input.current.value;
-      const valorType = type.current.value
-      getStar(valorBusca,valorType);
-      input.current.value = ""
-    }
-
-
-    return(
+  return (
     <>
-      <input className="border" type="text" placeholder="Faça sua pesquisa" ref={input}/>
-      <select ref={type}>
-      <option hidden value="">Escolha</option>
-      <option value="people">Personagem</option>
-      <option value="planets">Planeta</option>
+      <input
+        className="border"
+        type="text"
+        placeholder="Faça sua pesquisa"
+        ref={input}
+      />
+      <select ref={tipoRef}>
+        <option hidden value="">
+          Escolha
+        </option>
+        <option value="people">Personagem</option>
+        <option value="planets">Planeta</option>
         <option value="starships">Nave</option>
       </select>
-      <button className="border" onClick={handleClick} >Busca</button>
-      {(carregando) ? 
+      <button className="border" onClick={handleClick}>
+        Busca
+      </button>
+      {carregando ? (
         <h4 className=" font-bold">carregando...</h4>
-       : 
+      ) : (
         <h4 className=" font-bold">personagens</h4>
-      }
-        {personagens.map((personagem) => {
-          if(type.current.value ==="people"){
+      )}
+      {personagens.map((personagem) => {
+        if (type.current.value === "people") {
           return (
             <>
-              <div key={personagem.Id} className="box-border p-4 mb-4 border w-full">
-                <ul  >
-                  <li  className="font-bold">{personagem.Nome}</li>
-                  <li>{personagem.Altura}</li>
-                  <li>{personagem.Massa}</li>
-                  <li>{personagem.Cabelo}</li>
-                  <li>{personagem.gender}</li>
-                  <li>{personagem.URL}</li>                
+              <div
+                key={personagem.url}
+                className="box-border p-4 mb-4 border w-full"
+              >
+                <ul>
+                  <li className="font-bold">{personagem.name}</li>
+                  <li>Altura: {personagem.height}</li>
+                  <li>Massa: {personagem.mass}</li>
+                  <li>Cabelo: {personagem.hair_color}</li>
+                  <li>Gênero: {personagem.gender}</li>
+                  <li>URL: {personagem.url}</li>
                 </ul>
               </div>
             </>
           );
-        }else if(type.current.value ==="planets"){
+        } else if (type.current.value === "planets") {
           return (
             <>
-              <div key={personagem.Id} className="box-border p-4 mb-4 border w-full">
-                <ul  >
-                  <li className="font-bold">{personagem.Nome}</li>
-                  <li>{personagem.Rotacao}</li>
-                  <li>{personagem.Orbita}</li>             
-                  <li>{personagem.Diametro}</li>
-                  <li>{personagem.Clima}</li>
-                  <li>{personagem.URL}</li>   
+              <div
+                key={personagem.url}
+                className="box-border p-4 mb-4 border w-full"
+              >
+                <ul>
+                  <li className="font-bold">{personagem.name}</li>
+                  <li>Rotação: {personagem.rotation_period}</li>
+                  <li>Órbita: {personagem.orbital_period}</li>
+                  <li>Diâmetro: {personagem.diameter}</li>
+                  <li>Clima: {personagem.climate}</li>
+                  <li>URL: {personagem.url}</li>
                 </ul>
               </div>
             </>
           );
-        }
-        else{
+        } else {
           return (
             <>
-              <div key={personagem.Id} className="box-border p-4 mb-4 border w-full">
-                <ul  >
-                  <li className="font-bold">{personagem.Nome}</li>
-                  <li>{personagem.Modelo}</li>
-                  <li>{personagem.Fabricante}</li>
-                  <li>{personagem.Comprimento}</li>
-                  <li>{personagem.Capacidade}</li>
-                  <li>{personagem.URL}</li>   
+              <div
+                key={personagem.url}
+                className="box-border p-4 mb-4 border w-full">
+                <ul>
+                  <li className="font-bold">{personagem.name}</li>
+                  <li>Modelo: {personagem.model}</li>
+                  <li>Fabricante: {personagem.manufacturer}</li>
+                  <li>Comprimento: {personagem.length}</li>
+                  <li>Capacidade: {personagem.passengers}</li>
+                  <li>URL: {personagem.url}</li>
                 </ul>
               </div>
             </>
-          );          
+          );
         }
       })}
     </>
-    )
+  );
 }
 
-export default Lista
+export default Lista;
